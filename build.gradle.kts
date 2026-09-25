0fe9f72bdef57381a3f65432a8638421445d440e
mparvezmosharof84-creator <mparvezmosharof84@gmail.com>
2026-09-26 00:32:12 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,13 @@
 /*
  *  This file is part of AndroidIDE.
- *
- *  AndroidIDE is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  AndroidIDE is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
+ *  Complete Root Build Fix by Parvez Mosharof
  */
 
 @file:Suppress("UnstableApiUsage")
 
-import com.itsaky.androidide.build.config.BuildConfig
-import com.itsaky.androidide.build.config.FDroidConfig
-import com.itsaky.androidide.build.config.publishingVersion
 import com.itsaky.androidide.plugins.AndroidIDEPlugin
 import com.itsaky.androidide.plugins.conf.configureAndroidModule
 import com.itsaky.androidide.plugins.conf.configureJavaModule
-import com.itsaky.androidide.plugins.conf.configureMavenPublish
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -32,10 +16,9 @@ plugins {
   alias(libs.plugins.android.library) apply false
   alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.jvm) apply false
-  alias(libs.plugins.maven.publish) apply false
-  alias(libs.plugins.gradle.publish) apply false
   alias(libs.plugins.protobuf) apply false
   alias(libs.plugins.benchmark) apply false
+  id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10" apply false
 }
 
 buildscript {
@@ -45,26 +28,17 @@ buildscript {
   }
 }
 
-// Root project has 'com.itsaky.androidide' as the group ID
-project.group = BuildConfig.packageName
+// সেমভার এরর চিরতরে বন্ধ করার জন্য গ্লোবাল ভার্সন নির্ধারণ
+allprojects {
+  version = "v2.7.0-beta"
+}
 
 subprojects {
-  if (project != rootProject) {
-    var group = project.parent!!.group
-    if (project.parent != rootProject) {
-      group = "${group}.${project.parent!!.name}"
-    }
-    project.group = group
-  }
-
-  // Always load the F-Droid config
-  FDroidConfig.load(project)
-
   afterEvaluate {
     apply { plugin(AndroidIDEPlugin::class.java) }
   }
 
-  project.version = rootProject.version
+  project.version = "v2.7.0-beta"
 
   plugins.withId("com.android.application") {
     configureAndroidModule(libs.androidx.libDesugaring)
@@ -73,18 +47,10 @@ subprojects {
     configureAndroidModule(libs.androidx.libDesugaring)
   }
   plugins.withId("java-library") { configureJavaModule() }
-  plugins.withId("com.vanniktech.maven.publish.base") { configureMavenPublish() }
-
-  plugins.withId("com.gradle.plugin-publish") {
-    configure<GradlePluginDevelopmentExtension> {
-      version = project.publishingVersion
-    }
-  }
 
   tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-      jvmTarget = BuildConfig.javaVersion.toString()
-      freeCompilerArgs += "-Xstring-concat=inline"
+    compilerOptions {
+      freeCompilerArgs.addAll("-Xstring-concat=inline")
     }
   }
 }
