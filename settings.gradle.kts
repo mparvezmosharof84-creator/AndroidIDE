536f156a8d90f905c7f5c4e0869507c06dc1ad95
mparvezmosharof84-creator <mparvezmosharof84@gmail.com>
2026-09-26 01:25:02 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,18 @@
 /*
  *  This file is part of AndroidIDE.
- *  Complete Settings Fix Edition by Parvez Mosharof
+ *
+ *  AndroidIDE is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  AndroidIDE is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 pluginManagement {
@@ -10,6 +22,7 @@ pluginManagement {
     google()
     mavenCentral()
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
     maven("https://jitpack.io")
   }
 }
@@ -25,6 +38,8 @@ dependencyResolutionManagement {
   }
 }
 
+// এই দুটি কম্পোজিট বিল্ড থাকা আবশ্যক যাতে javac, javapoet, appintro ইত্যাদি সব পাওয়া যায়
+includeBuild("composite-builds/build-deps")
 includeBuild("composite-builds")
 
 rootProject.name = "AndroidIDE"
